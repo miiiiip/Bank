ee517ec524888b3f033750171b36c7e7318a80d5
Kahnurd <hetti031@morris.umn.edu>
2022-11-30 13:59:49 -0600

    Add constructor for RSA for bit length and tried message Digest
    Message Digest doesn't do anything yet, its just for testing.

--- Bank.java
+++ Bank.java
@@ -1,3 +1,23 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Bank {
     
+
+    public static void main(String[] args) {
+        try{
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] potato = new byte[2];
+            potato[0] = (byte)74;
+            potato[1] = (byte)108;
+            byte[] bean = digest.digest(potato);
+            for (int i = 0; i < bean.length; i++) {
+                System.out.println(bean[i]);
+            }
+            System.out.println(bean.length);
+            System.out.flush();
+    
+    } catch(NoSuchAlgorithmException e) {System.out.println(e);}
+        
+    }
 }
--- RSA.java
+++ RSA.java
@@ -1,4 +1,5 @@
 import java.math.BigInteger;
+import java.util.Random;
 
 public class RSA {
     BigInteger e;
@@ -25,6 +26,17 @@ public class RSA {
         d = e.modInverse(phiN);
     }
 
+    public RSA(int bitLength, Random rnd) {
+        q = BigInteger.probablePrime(bitLength, rnd);
+        p = BigInteger.probablePrime(bitLength, rnd);
+        n = p.multiply(q);
+        phiN = (p.subtract(new BigInteger("1"))).multiply(q.subtract(new BigInteger("1")));
+        e = q.nextProbablePrime();
+        d = e.modInverse(phiN);
+    }
+
+
+
     public BigInteger encrypt(String input) {
         BigInteger bigInput = new BigInteger(input);
         BigInteger enc = bigInput.modPow(e, n);
@@ -44,9 +56,14 @@ public class RSA {
     }
 
     public static void main(String[] args) {
-        RSA cringe = new RSA(492901573, 507435167);
+        RSA cringe = new RSA(500, new Random());
+        System.out.println(cringe.e);
+        System.out.println(cringe.d);
+        System.out.println(cringe.p);
+        System.out.println(cringe.q);
+        
         // System.out.println("Encrypt: " + RSA.encryptSpecific("102734180247043",
         // "664725562992008719", "3"));
-        System.out.println("Decrypt: " + cringe.decrpyt(new BigInteger("89497323804483257")));
+        //System.out.println("Decrypt: " + cringe.decrpyt(new BigInteger("89497323804483257")));
     }
 }
